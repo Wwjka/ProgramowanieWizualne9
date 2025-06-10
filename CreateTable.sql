b5339a0652b7d7ac7da12cf4874f3d34f38682d4
Wwjka <84907257+Wwjka@users.noreply.github.com>
2025-06-10 12:39:17 +0200

    Add files via upload

---
+++ CreateTable.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Wnioski (
+    Kolumna1 NVARCHAR(255),
+    Kolumna2 NVARCHAR(255),
+    Kolumna3 NVARCHAR(255),
+    Kolumna4 NVARCHAR(255),
+    Kolumna5 NVARCHAR(255),
+    Kolumna6 NVARCHAR(255),
+    Kolumna7 NVARCHAR(255),
+    Kolumna8 NVARCHAR(255),
+    Kolumna9 NVARCHAR(255),
+    Kolumna10 NVARCHAR(255),
+    Kolumna11 NVARCHAR(255),
+    Kolumna12 NVARCHAR(255),
+    Kolumna13 NVARCHAR(255),
+    Kolumna14 NVARCHAR(255),
+    Kolumna15 NVARCHAR(255)
+);
